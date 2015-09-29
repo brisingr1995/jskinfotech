295049355eb4c6562791a2a86415e6d38b1659d7
Lionel Dsilva <lionelcd94@gmail.com>
2015-09-29 23:51:55 +0530

    cleanup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
-		@posts = Post.all.limit(3).order("created_at desc")
+		@posts = Post.all.order("title asc")
 		@projects = Project.all.limit(3).order("created_at desc")
 	end
 end
